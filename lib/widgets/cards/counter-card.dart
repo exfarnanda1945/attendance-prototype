b831a7061409a68acc58dfdf65b432a83bce78ab
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-02-11 14:19:37 +0700

    add: counter card

---
+++ lib/widgets/cards/counter-card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CounterCard extends StatelessWidget {
+  const CounterCard({super.key, required this.title, required this.count});
+
+  final String title;
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 1,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(10))),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+              ),
+            ),
+            Text(
+              count.toString(),
+              textAlign: TextAlign.end,
+              style:
+              const TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+            )
+          ],
+        ),
+      ),
+    );;
+  }
+}
