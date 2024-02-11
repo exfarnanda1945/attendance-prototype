d83e177fd4857a7a379aae8a1a5306f58d5f7e05
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-02-11 14:21:18 +0700

    add: ofw and ofo route

--- lib/routes/app-router.dart
+++ lib/routes/app-router.dart
@@ -1,19 +1,21 @@
+import 'package:attendance_prototype/pages/approval/ofo/ofo-add-edit-page.dart';
+import 'package:attendance_prototype/pages/approval/ofo/ofo-approval-page.dart';
+import 'package:attendance_prototype/pages/approval/ofw/ofw-add-edit-page.dart';
+import 'package:attendance_prototype/pages/approval/ofw/ofw-approval-screen.dart';
+import 'package:attendance_prototype/pages/approval/sick/sick-add-edit-page.dart';
+import 'package:attendance_prototype/pages/approval/sick/sick-page.dart';
 import 'package:attendance_prototype/pages/attendance/attendance.page.dart';
 import 'package:attendance_prototype/pages/auth/sign_in.page.dart';
 import 'package:attendance_prototype/pages/auth/sign_up.page.dart';
 import 'package:attendance_prototype/pages/main/main.page.dart';
 import 'package:attendance_prototype/pages/manual-attendance/manual_attendance.page.dart';
-import 'package:attendance_prototype/pages/sick-approval/sick-add-edit-page.dart';
-import 'package:attendance_prototype/pages/sick-approval/sick-page.dart';
 import 'package:attendance_prototype/utils/constant.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static GoRouter getRoutes(bool isUserAlreadyLogin) {
     return GoRouter(
-        initialLocation: isUserAlreadyLogin
-            ? "/approval/sick"
-            : '/signIn',
+        initialLocation: isUserAlreadyLogin ? "/approval/ofo" : '/signIn',
         debugLogDiagnostics: true,
         routes: [
           GoRoute(
@@ -73,12 +75,48 @@ class AppRouter {
             name: "sick",
             path: "/approval/sick",
             routes: [
-              GoRoute(path: "add-edit",name: "sick-add-edit",builder: (context, state) {
-                return const SickAddEditPage();
-              },)
+              GoRoute(
+                path: "add-edit",
+                name: "sick-add-edit",
+                builder: (context, state) {
+                  return const SickAddEditPage();
+                },
+              )
             ],
             builder: (context, state) {
               return const SickPage();
+            },
+          ),
+          GoRoute(
+            name: "ofw",
+            path: "/approval/ofw",
+            routes: [
+              GoRoute(
+                path: "add-edit",
+                name: "ofw-add-edit",
+                builder: (context, state) {
+                  return const OfwAddEditPage();
+                },
+              )
+            ],
+            builder: (context, state) {
+              return const OfwApprovalScreen();
+            },
+          ),
+          GoRoute(
+            name: "ofo",
+            path: "/approval/ofo",
+            routes: [
+              GoRoute(
+                path: "add-edit",
+                name: "ofo-add-edit",
+                builder: (context, state) {
+                  return const OfoAddEditPage();
+                },
+              )
+            ],
+            builder: (context, state) {
+              return const OfoApprovalPage();
             },
           ),
         ]);
