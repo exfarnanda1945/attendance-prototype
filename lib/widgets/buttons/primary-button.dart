bc51a499e03a94bc4e4fe3905dbf1bfc0ade127c
miko <exfar@samir.co.id>
2024-02-16 15:29:58 +0700

    add: primary button

---
+++ lib/widgets/buttons/primary-button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  PrimaryButton({super.key,
+    required this.title,
+    required this.onPressed,
+    this.height = 48,
+    this.width = double
+        .infinity,});
+
+  final String title;
+  final void Function() onPressed;
+
+  double? height;
+  double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height:height,
+      width: width,
+      child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme
+                .of(context)
+                .colorScheme
+                .primary,
+            foregroundColor: Theme
+                .of(context)
+                .colorScheme
+                .onPrimary,
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+          child: Text(title,style: const TextStyle(
+            fontWeight: FontWeight.w500,fontSize: 15
+          ),)),
+    );
+  }
+}
